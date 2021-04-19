185685d187d060e855ad63be2c91a605366fc550
Christian Dupuis <cd@atomist.com>
2021-04-19 13:15:18 +0200

    Add logging

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -17,16 +17,20 @@
 import * as express from "express";
 
 function loggingMiddleware(): void {
+	console.log("Initialising logging middleware");
+
 	const { post: originalPost } = express.application;
 
 	const middleware = (req, res, next) => {
+		console.log("Inside middleware");
 		const traceId = req.get("x-cloud-trace-context");
 		const executionId = req.get("function-execution-id");
 		setTraceIds(traceId, executionId);
 		next();
 	};
 
-	express.application.post = function post(path, ...rest) {
+	express.application.post = (path, ...rest) => {
+		console.log("Inside post");
 		this.use(middleware);
 		return originalPost.bind(this)(path, ...rest);
 	};
@@ -37,6 +41,7 @@ loggingMiddleware();
 let traceIds;
 
 function setTraceIds(traceId: string, executionId: string): void {
+	console.log("setTraceIds %s %s", traceId, executionId);
 	traceIds = {
 		traceId,
 		executionId,
@@ -44,9 +49,11 @@ function setTraceIds(traceId: string, executionId: string): void {
 }
 
 export function clearTraceIds(): void {
+	console.log("clearTraceIds");
 	traceIds = undefined;
 }
 
 export function getTraceIds(): { traceId: string; executionId: string } {
+	console.log("getTraceIds %s", JSON.stringify(traceIds || {}));
 	return traceIds;
 }
